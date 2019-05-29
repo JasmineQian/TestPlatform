cbb4898f8a0a7821302e17d32eb1c330b8362187
Jasmine QIAN <jasqia@acxiom.com>
2019-05-29 14:23:14 +0800

    add about

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -9,15 +9,27 @@ public class HomeController {
 
 
     @GetMapping({"/", "/index", "/home"})
-    public String root() {
+    public String index() {
         return "index";
     }
 
     @GetMapping("/underBuild")
-    public String casegenerate() {
+    public String underBuild() {
         return "underBuild";
     }
 
+    @GetMapping("/news")
+    public String news() {return "about/news";}
+
+    @GetMapping("/contact")
+    public String contact() {return "about/contact";}
+
+    @GetMapping("/guide")
+    public String guide() {return "about/guide";}
+
+
+    @GetMapping("/donate")
+    public String donate() {return "about/donate";}
 
 
 }
